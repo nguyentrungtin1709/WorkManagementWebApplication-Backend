08dc0d964be33217c7053f3183ee4a02a2502b7d
Lyridsss <trungtincom7@gmail.com>
2024-02-18 21:23:30 +0700

    Chỉnh sửa

--- src/main/java/com/application/WorkManagement/dto/requests/table/TableEntityRequest.java
+++ src/main/java/com/application/WorkManagement/dto/requests/table/TableEntityRequest.java
@@ -14,7 +14,7 @@ import org.hibernate.validator.constraints.Length;
 public class TableEntityRequest {
 
     @NotBlank(message = "Không được bỏ trống")
-    @Length(max = 120, message = "Tên không gian làm việc không được vượt quá 120 ký tự")
+    @Length(max = 120, message = "Tên bảng không được vượt quá 120 ký tự")
     private String name;
 
     @Length(max = 1000, message = "Mô tả không được vượt quá 1000 ký tự")
